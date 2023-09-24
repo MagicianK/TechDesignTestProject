bddc43695c17fc8cdfe3331a11d8a61ddd73cece
MagzhanK <magzhan.k.1000@gmail.com>
2023-09-24 12:39:06 +0600

    Audio sources

--- Assets/Scripts/ActivateButtonOnClicked.cs
+++ Assets/Scripts/ActivateButtonOnClicked.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Localization.Plugins.XLIFF.V12;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ActivateButtonOnClicked : MonoBehaviour
 {
     public Button button;
-
+    public AudioSource source;
     private void Start()
     {
         button.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
@@ -16,5 +17,6 @@ public class ActivateButtonOnClicked : MonoBehaviour
     {
         button.interactable = true;
         button.GetComponentInChildren<TextMeshProUGUI>().enabled = true;
+        source.Play();
     }
 }
--- Assets/Scripts/PlayAnimationOnClicked.cs
+++ Assets/Scripts/PlayAnimationOnClicked.cs
@@ -6,9 +6,10 @@ public class PlayAnimationOnClicked : MonoBehaviour
 {
     public Animator animator;
     public string animation_name;
-
+    public AudioSource source;
     private void OnMouseDown()
     {
         animator.Play(animation_name);
+        source.Play();
     }
 }
